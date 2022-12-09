e898f86d3dd341f055dfee0affaabc0ca4a741fb
Marcus <cheney.chiu@outlook.com>
2022-12-08 21:58:34 -0500

    update

--- setup.sh
+++ setup.sh
@@ -9,14 +9,14 @@ bash Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 
 # Create a new conda environment named "my_env" with Python 3.7
-conda create -n cs655 django
+conda create -n cs django
 
 # Activate the conda environment
-conda activate cs655
+conda activate cs
 
 # Install the "djangorestframework" and "pytorch" packages in the environment
 conda install -c conda-forge djangorestframework
 conda install pytorch torchvision torchaudio cpuonly -c pytorch
 
 # run the server
-python manage.py runserver 0.0.0.0:8000
+nohup python manage.py runserver 0.0.0.0:8000 &
