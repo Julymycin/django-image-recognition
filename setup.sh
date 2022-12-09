c08a6fdba2f0d5a7ffdc851d55ddb38ae005205e
Marcus <cheney.chiu@outlook.com>
2022-12-08 21:10:34 -0500

    upload setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Download the Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Install Miniconda
+bash Miniconda3-latest-Linux-x86_64.sh
+
+# Create a new conda environment named "my_env" with Python 3.7
+conda create -n cs655 django
+
+# Activate the conda environment
+conda activate cs655
+
+# Install the "djangorestframework" and "pytorch" packages in the environment
+conda install -c conda-forge djangorestframework
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
+
+# git clone the repo
+git clone https://github.com/Julymycin/django-image-recognition.git
+
+# enter the folder
+cd django-image-recognition
+
+# run the server
+python manage.py runserver 0.0.0.0:8000
